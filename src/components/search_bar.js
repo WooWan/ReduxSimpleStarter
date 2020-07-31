76653407537881bb277f013b3b5db0d6b13bfb83
WooWan <woohobi@gmail.com>
2020-07-31 14:10:49 +0900

    react study src update

---
+++ src/components/search_bar.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+//{Component}의 의미: 앞의 React 라이브러리에서 component 프로퍼티를 가져와라
+class SearchBar extends Component{
+    render(){
+        return <input />
+    }
+    //class를 생성 해줄때마다 render랑 jsx를 변환해줘야한다
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
+//Create a new Component. This component should produce
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react';
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyCUKZwcpomrkOozjgKGumbeA_-Cu-z1Jjg';
+const { divide } = require('lodash')
 
-import App from './components/app';
-import reducers from './reducers';
+//some HTML
+const App =() => {//화살표는 함수 정의와 동일하다
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
+    
+    //jsx html을 js에 쓸수 있도록 사용
+    //jsx의 목적 : html로 변환되어 페이지의 dom으로 삽입
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// dom에 렌더링하기 전에 컴포넌트(클래스)를 인스턴스화해야한다
+//<App></App> 이런식으로도 가능
+ReactDOM.render(<App />,document.querySelector('.container'));
+//render은 넘기는 요소와 어디에 넘길지도 설정해주어야한다
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's generated HTML and pui it
+//on page
